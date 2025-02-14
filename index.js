028174b3226ab6146b87b902e8b003dd12934899
Aakashdeep Singh <122137207+aakashdeep8530@users.noreply.github.com>
2025-02-14 11:40:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-// index.js
-
-console.log("Hello, welcome to my project!");
-
-// Example function
-function add(a, b) {
-  return a + b;
-}
-
-console.log(add(5, 7));
+// hello.js
+console.log("Hello, World!");
